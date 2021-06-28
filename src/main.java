9c7ca371756a2582d39d1cbd6c66d29c26032252
ayonthuhasha <thuhasha@gmail.com>
2021-06-28 19:07:01 +0530

    main method update

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ import static model.AppointmentModel.writingTXT;
 
 public class main {
     public static void main(String[] args) {
-        writingTXT("xyz");
+        writingTXT("abc");
     }
 }
